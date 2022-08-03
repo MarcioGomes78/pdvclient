20a2bb3a5dc1192dd0c8b9eadf75f29f9f3e9984
MarcioGomes78 <mjgomes1978@gmail.com>
2022-08-02 22:36:18 -0300

    Implementando classes: Estado e Cidade.

---
+++ src/main/java/com/mjgomes/pdvclient/domain/Cidade.java
@@ -0,0 +1,58 @@
+package com.mjgomes.pdvclient.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Cidade implements Serializable {
+
+    private Long id;
+    private String nome;
+
+    private Estado estado;
+
+    public Cidade() {
+    }
+
+    public Cidade(Long id, String nome, Estado estado) {
+        this.id = id;
+        this.nome = nome;
+        this.estado = estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cidade)) return false;
+        Cidade cidade = (Cidade) o;
+        return Objects.equals(id, cidade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/mjgomes/pdvclient/domain/Estado.java
@@ -0,0 +1,47 @@
+package com.mjgomes.pdvclient.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Estado implements Serializable {
+
+    private Long id;
+    private String nome;
+
+    public Estado() {
+    }
+
+    public Estado(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Estado)) return false;
+        Estado estado = (Estado) o;
+        return Objects.equals(id, estado.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
